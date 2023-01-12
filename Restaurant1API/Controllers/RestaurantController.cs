d8fe66339a148709a0b275bcbb470c71cf10837d
Rafael Milewski <rafimilewski@hotmail.com>
2023-01-12 14:27:02 +0200

    Add project files.

---
+++ Restaurant1API/Controllers/RestaurantController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant1API.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Restaurant1API.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+       public RestaurantController(RestaurantDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+    public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restaurants = _dbContext
+                            .Restaurants
+                            .ToList();
+
+
+            return Ok(restaurants);
+        }
+    }
+}
